4db84106ce93168942fe4ac8d7adbe1777616bc5
Flesh <olivia.a.gowan@gmail.com>
2024-03-18 17:47:49 -0400

    responsive bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -2,9 +2,7 @@ function BioComponent() {
   return (
     <div>
       <section className="card bio-card row">
-        <div className="bio-photo col">
           <img src="src/assets/profilephoto.jpg" alt="Olivia Gowan Photo" />
-        </div>
         <div className="bio-text col">
           <h1>About Me</h1>
           <p>My name is Olivia Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum saepe inventore, corrupti autem voluptatum officiis ipsum quis asperiores cupiditate, iusto temporibus est odio commodi? Natus sequi assumenda totam! Dolorem, molestiae?</p>
