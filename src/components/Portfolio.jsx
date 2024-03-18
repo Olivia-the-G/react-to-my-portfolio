c22999b0b30afe71ce3c242bff23bde50f65a982
Flesh <olivia.a.gowan@gmail.com>
2024-03-18 19:00:22 -0400

    projects styled

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,7 @@ import ProjectComponent from "./Project";
 function PortfolioComponent() {
   return (
     <div>
-      <section className="card portfolio-card">
+      <section className="card portfolio-card row">
         <h1>My Projects</h1>
         <ProjectComponent />
       </section>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,30 +4,32 @@ function ProjectComponent() {
       title: "Blogger",
       deployedUrl: "https://blogger-like-frogger-59f0220c1556.herokuapp.com/",
       gitHubUrl: "https://github.com/Olivia-the-G/blogger",
-      image: "blogger-like-frogger.png",
+      image: "/src/assets/blogger-like-frogger.png",
     },
     {
       title: "Hungry Mikey",
       deployedUrl: "https://hungry-hungry-mikey-f614bbce79a1.herokuapp.com/game",
       gitHubUrl: "https://github.com/Olivia-the-G/Hungry_Mikey",
-      image: "hungry-hungry-mikey.png",
+      image: "/src/assets/hungry-mikey.png",
     },
     {
       title: "Weather Dashy",
       deployedUrl: "https://olivia-the-g.github.io/weather-dashy/",
       gitHubUrl: "https://github.com/Olivia-the-G/weather-dashy",
-      image: "weather-dashy.png",
+      image: "/src/assets/weather-dashy.png",
     }
   ];
 
   return (
-    <div>
+    <div className="row">
       {projects.map((project, index) => (
         <section key={index} className="card project-card col">
-          <h1>{project.title}</h1>
+          <h2>{project.title}</h2>
           <img src={project.image} alt={project.title} />
-          <a className="hide" href={project.deployedUrl}>Deployed Project</a>
-          <a className="hide" href={project.gitHubUrl}>Github Repo</a>
+          <div className="overlay">
+            <a className="overlay-text" href={project.deployedUrl}>Deployed Project</a>
+            <a className="overlay-text" href={project.gitHubUrl}>Github Repo</a>
+          </div>
         </section>
       ))}
     </div>
