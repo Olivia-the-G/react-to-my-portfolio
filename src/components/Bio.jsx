29e63e3bd70729171bf354130225ef3031c2a4a3
Flesh <olivia.a.gowan@gmail.com>
2024-03-20 17:26:09 -0400

    formatted resume card

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -2,7 +2,7 @@ function BioComponent() {
   return (
     <div>
       <section className="card bio-card row">
-          <img src="src/assets/profilephoto.jpg" alt="Olivia Gowan Photo" />
+          <img src="/src/assets/profilephoto.jpg" alt="Olivia Gowan Photo" />
         <div className="bio-text col">
           <h1>About Me</h1>
           <p>My name is Olivia Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum saepe inventore, corrupti autem voluptatum officiis ipsum quis asperiores cupiditate, iusto temporibus est odio commodi? Natus sequi assumenda totam! Dolorem, molestiae?</p>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,8 +3,37 @@ function ResumeComponent() {
     <div>
       <section className="card resume-card col">
         <h1>Resume</h1>
-        
+        <a href="/src/assets/resume.pdf" download>Download Resume</a>
 
+        <div className="download-box row"> 
+          <div className="box">
+            <h2>Front-End Proficiencies</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>JQuery</li>
+              <li>Responsive Design</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+              <li>Handlebars</li>
+            </ul>
+          </div>
+          <div className="box">
+            <h2>Back-End Proficiencies</h2>
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL</li>
+              <li>Sequelize</li>
+              <li>NoSQL</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>REST</li>
+            </ul>
+          </div>
+        </div>
       </section>
     </div>
   );
