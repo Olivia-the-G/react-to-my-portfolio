268e9ae03c990cb0c4d4e3c8b735117f4c8a0e1c
Flesh <olivia.a.gowan@gmail.com>
2024-03-20 20:35:22 -0400

    fixed formatting

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function NavComponent() {
     <>
       <nav>
         <ul className="row">
-          <li><NavLink to="/" >About Me</NavLink></li>
+          <li><NavLink to="/">About Me</NavLink></li>
           <li><NavLink to="/portfolio">Portfolio</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
           <li><NavLink to="/resume">Resume</NavLink></li>
