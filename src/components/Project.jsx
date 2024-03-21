6063d042b53f2db817d28034759c6ee6cf5418d8
Flesh <olivia.a.gowan@gmail.com>
2024-03-20 23:21:50 -0400

    moved assets to root

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,37 +7,37 @@ function ProjectComponent() {
       title: "Blogger",
       deployedUrl: "https://blogger-like-frogger-59f0220c1556.herokuapp.com/",
       gitHubUrl: "https://github.com/Olivia-the-G/blogger",
-      image: "public/blogger-like-frogger.png",
+      image: "/assets/blogger-like-frogger.png",
     },
     {
       title: "Hungry Mikey",
       deployedUrl: "https://hungry-hungry-mikey-f614bbce79a1.herokuapp.com/game",
       gitHubUrl: "https://github.com/Olivia-the-G/Hungry_Mikey",
-      image: "/src/assets/hungry-mikey.png",
+      image: "/assets/hungry-mikey.png",
     },
     {
       title: "Weather Dashy",
       deployedUrl: "https://olivia-the-g.github.io/weather-dashy/",
       gitHubUrl: "https://github.com/Olivia-the-G/weather-dashy",
-      image: "/src/assets/weather-dashy.png",
+      image: "/assets/weather-dashy.png",
     },
     {
       title: "Kumamo",
       deployedUrl: "https://olivia-the-g.github.io/culinary_curation_app/",
       gitHubUrl: "https://github.com/Olivia-the-G/culinary_curation_app",
-      image: "/src/assets/kumamo.png",
+      image: "/assets/kumamo.png",
     },
     {
       title: "My First Portfolio Site",
       deployedUrl: "https://olivia-the-g.github.io/Professional-Portfolio/#",
       gitHubUrl: "https://github.com/Olivia-the-G/Professional-Portfolio",
-      image: "/src/assets/professional-portfolio.png",
+      image: "/assets/professional-portfolio.png",
     },
     {
       title: "Notary Express",
       deployedUrl: "https://floating-lowlands-42980-b282d39f2668.herokuapp.com/",
       gitHubUrl: "https://github.com/Olivia-the-G/notary-express",
-      image: "/src/assets/notary-express.png",
+      image: "/assets/notary-express.png",
     }
   ];
 
