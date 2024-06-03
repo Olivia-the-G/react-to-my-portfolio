5c66d753931de3875c11a1b07d932bc86e4b55d8
Flesh <olivia.a.gowan@gmail.com>
2024-06-03 13:14:36 -0400

    fixed resizing bug

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -46,12 +46,12 @@ function ProjectComponent({ deviceType }) {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
-      slidesToSlide: 3 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -69,8 +69,8 @@ function ProjectComponent({ deviceType }) {
         responsive={responsive}
         ssr={true} // means to render carousel on server-side.
         infinite={true}
-        autoPlay={deviceType !== "mobile"}
-        autoPlaySpeed={1000}
+        // autoPlay={deviceType !== "mobile"}
+        // autoPlaySpeed={1000}
         keyBoardControl={true}
         customTransition="all .5"
         transitionDuration={500}
